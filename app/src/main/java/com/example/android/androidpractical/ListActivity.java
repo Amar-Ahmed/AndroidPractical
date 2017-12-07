3495cd401b7eb059deb304e33f0c2ca6d393f042
Amar Ahmed <amar11432@gmail.com>
2017-12-06 21:19:04 -0500

    added part03

--- app/src/main/java/com/example/android/androidpractical/ListActivity.java
+++ app/src/main/java/com/example/android/androidpractical/ListActivity.java
@@ -1,13 +1,21 @@
 package com.example.android.androidpractical;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ListActivity extends AppCompatActivity {
+    private SharedPreferences loginPreferences;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        String username = loginPreferences.getString("username", "");
+        String password = loginPreferences.getString("password", "");
     }
+
 }
