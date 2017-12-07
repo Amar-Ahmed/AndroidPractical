2deed4b8658391b295fe330daec9dfb697c3304a
Amar Ahmed <amar11432@gmail.com>
2017-12-06 21:15:14 -0500

    added part02

--- app/src/main/java/com/example/android/androidpractical/LoginActivity.java
+++ app/src/main/java/com/example/android/androidpractical/LoginActivity.java
@@ -1,13 +1,79 @@
 package com.example.android.androidpractical;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String username, password;
+    private Button ok;
+    private EditText editTextUsername, editTextPassword;
+    private CheckBox saveLoginCheckBox;
+    private SharedPreferences loginPreferences;
+    private SharedPreferences.Editor loginPrefsEditor;
+    private Boolean saveLogin;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ok = (Button) findViewById(R.id.buttonLogin);
+        ok.setOnClickListener(this);
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        saveLoginCheckBox = (CheckBox) findViewById(R.id.saveLoginCheckBox);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+        if (saveLogin == true) {
+            editTextUsername
+                    .setText(loginPreferences.getString("username", ""));
+            editTextPassword
+                    .setText(loginPreferences.getString("password", ""));
+            saveLoginCheckBox.setChecked(true);
+        }
+    }
+
+    public void onClick(View view) {
+        if (view == ok) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(editTextUsername.getWindowToken(), 0);
+
+            username = editTextUsername.getText().toString();
+            password = editTextPassword.getText().toString();
+
+            if (saveLoginCheckBox.isChecked()&& username.equals("user@aol.com") && password.equals("password1234")) {
+                loginPrefsEditor.putBoolean("saveLogin", true);
+                loginPrefsEditor.putString("username", username);
+                loginPrefsEditor.putString("password", password);
+                loginPrefsEditor.commit();
+            } else {
+                loginPrefsEditor.clear();
+                loginPrefsEditor.commit();
+            }
+
+            doSomethingElse();
+        }
+    }
+
+    public void doSomethingElse() {
+        startActivity(new Intent(LoginActivity.this, ListActivity.class));
+        LoginActivity.this.finish();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
